4a22a4a5489faacf91441d3d0d5296fa3cbebe12
kuckjwi <rnrghks09@gmail.com>
2022-08-13 18:52:08 +0900

    print to b3 propagation

--- echo-server/src/main/kotlin/com/kuckjwi/spring/cloud/echo/server/controller/EchoController.kt
+++ echo-server/src/main/kotlin/com/kuckjwi/spring/cloud/echo/server/controller/EchoController.kt
@@ -2,6 +2,7 @@ package com.kuckjwi.spring.cloud.echo.server.controller
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.cloud.sleuth.Tracer
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -9,12 +10,15 @@ import javax.servlet.http.HttpServletRequest
 
 @RestController
 @RequestMapping("/echo")
-class EchoController {
+class EchoController(private val tracer: Tracer) {
   private val log: Logger = LoggerFactory.getLogger(EchoController::class.java)
 
   @GetMapping
   fun echo(request: HttpServletRequest): String {
-    log.info("{}", request.contextPath)
+    // B3 propagation check
+    log.info("x-b3-traceid: {}", request.getHeader("x-b3-traceid"))
+    log.info("x-b3-parentspanid:: {}", request.getHeader("x-b3-parentspanid"))
+    log.info("x-b3-spanid: {}", request.getHeader("x-b3-spanid"))
     return "echo"
   }
 }
